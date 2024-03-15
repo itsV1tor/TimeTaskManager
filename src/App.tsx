a13e7055df4abfb1cc61ddf4d84f7649ee1be3ca
H4KV <vitor.sannctorum@hotmail.com>
2024-03-15 17:52:22 -0300

    Update: Timer play button configured

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import { Tasks } from './components/Tasks';
 export function App() {
   return (
     <div className="grid h-screen w-full place-items-center p-8 antialiased">
-      <div className="w-full max-w-[1080px] rounded-md border p-8 flex flex-col gap-8">
+      <div className="flex w-full max-w-[1080px] flex-col gap-8 rounded-md border p-8">
         <Header />
-        <Timer/>
-        <Tasks/>
+        <Timer />
+        <Tasks />
       </div>
     </div>
   );
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,14 +1,63 @@
 import { FaPlay, FaPause, FaStop } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
 
 export function Timer() {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(59);
+  const [seconds, setSeconds] = useState(55);
+  const [isRunning, setIsRunning] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    let interval: undefined | number;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => {
+          if (seconds >= 59) {
+            setSeconds((seconds) => (seconds = 0));
+            setMinutes((minutes) => {
+              if (minutes >= 59) {
+                setIsVisible(true);
+                setMinutes((minutes) => (minutes = 0));
+                setHours((hours) => {
+                  return hours + 1;
+                });
+              }
+              return minutes + 1;
+            });
+          }
+          return seconds + 1;
+        });
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+  }, [isRunning]);
+
+  const visibilityHours = () => {
+    return isVisible === true ? '' : 'hidden';
+  };
+
+  const padWithZero = (number: number) => {
+    return number < 10 ? '0' + number : number;
+  };
+
+  const startTimer = () => {
+    if (isRunning) {
+      return;
+    }
+    setIsRunning(true);
+  };
+
   return (
     <div className="m-auto flex w-full max-w-[720px] flex-col items-center gap-4">
-      <p className="text-5xl text-center font-medium text-zinc-800">
-        <span>00</span>:<span>00</span>
+      <p className="text-center text-5xl font-medium text-zinc-800">
+        <span className={visibilityHours()}>{padWithZero(hours)}:</span>
+        <span>{padWithZero(minutes)}</span>:<span>{padWithZero(seconds)}</span>
       </p>
 
-      <div className="flex gap-8 items-center">
-        <button>
+      <div className="flex items-center gap-8">
+        <button onClick={startTimer}>
           <FaPlay className="fill-zinc-400 transition duration-300 hover:fill-zinc-800" />
         </button>
         <button>
